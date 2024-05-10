101b616d1b35ed89f0c2ee6e576a68cfb5dd54f7
Lior1254 <liorshemtov2008@gmail.com>
2024-05-11 00:17:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/switchandradiogroup/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.switchandradiogroup;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioButton rb1,rb2,rb3,rb4;
+    LinearLayout back;
+    Switch Sw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        weddings();
+    }
+    public void weddings()
+    {
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        rb3 = findViewById(R.id.rb3);
+        rb4 = findViewById(R.id.rb4);
+        back = findViewById(R.id.back);
+        Sw = findViewById(R.id.Sw);
+    }
+
+    public void option(View view)
+    {
+        if(Sw.isChecked())
+        {
+            colors();
+        }
+    }
+
+    public void btn_change(View view)
+    {
+        colors();
+    }
+    public void colors()
+    {
+        if(rb1.isChecked())
+        {
+            back.setBackgroundColor(Color.RED);
+        }
+        else if(rb2.isChecked())
+        {
+            back.setBackgroundColor(Color.GREEN);
+        }
+        else if(rb3.isChecked())
+        {
+            back.setBackgroundColor(Color.BLUE);
+        }
+        else
+        {
+            back.setBackgroundColor(Color.rgb(234,223,131));
+        }
+    }
+
+}
